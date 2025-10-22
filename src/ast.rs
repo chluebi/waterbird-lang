a2693bdab22b38f9ccab33aadf1ba624e906709c
chluebi <42419603+chluebi@users.noreply.github.com>
2025-10-23 00:57:03 +0200

    printing types

--- src/ast.rs
+++ src/ast.rs
@@ -1,10 +1,25 @@
+use std::fmt;
 use std::ops::Range;
 use std::collections::HashMap;
 
 pub type Loc = Range<usize>;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct KeywordArgumentType {
+    name: String,
+    arg_type: Box<Type>
+}
+
+impl fmt::Display for KeywordArgumentType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}={}", self.name, self.arg_type)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Type {
     Generic(String),
+    Void,
     Int32,
     Int64,
     Bool,
@@ -14,11 +29,81 @@ pub enum Type {
     Dict {
         keys: Box<Type>,
         values: Box<Type>
+    },
+    Callable {
+        generics: Vec<String>,
+        positional_arguments: Vec<Type>,
+        variadic_argument: Box<Option<Type>>,
+        keyword_arguments: Vec<KeywordArgumentType>,
+        keyword_variadic_argument: Box<Option<Type>>,
+        return_type: Box<Type>
     }
+}
+
+impl fmt::Display for Type {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Type::Generic(s) => write!(f, "{}", s),
+            Type::Void => write!(f, "()"),
+            Type::Int32 => write!(f, "int"),
+            Type::Int64 => write!(f, "int64"),
+            Type::Bool => write!(f, "bool"),
+            Type::Str => write!(f, "str"),
+            Type::Tuple(type_vec) => {
+                type_vec.iter().map(|t| write!(f, "{}", t)).collect()
+            },
+            Type::List(t) => write!(f, "List<{}>", t),
+            Type::Dict{keys, values} => write!(f, "Dict<{},{}>", keys, values),
+            Type::Callable{generics, positional_arguments, variadic_argument, keyword_arguments, keyword_variadic_argument, return_type} => {
+                write!(f, "fn")?;
+
+                // Generics, if any
+                if !generics.is_empty() {
+                    let gens = generics.join(", ");
+                    write!(f, "(gen: {}; ", gens)?;
+                } else {
+                    write!(f, "(")?;
+                }
+
+                // Positional args
+                let mut parts = vec![];
+
+                for t in positional_arguments {
+                    parts.push(format!("{}", t));
+                }
+
+                // *args
+                if let Some(var) = variadic_argument.as_ref() {
+                    parts.push(format!("*{}", var));
+                }
+
+                // keyword args
+                for kw in keyword_arguments {
+                    parts.push(format!("{}", kw));
+                }
+
+                // **kwargs
+                if let Some(kv) = keyword_variadic_argument.as_ref() {
+                    parts.push(format!("**{}", kv));
+                }
+
+                write!(f, "{}", parts.join(", "))?;
+
+                // Close fn args
+                write!(f, ") -> {}", return_type)
+            }         
+        }
+    }
+}
+
+pub struct KeywordArgumentTypeLiteral {
+    name: String,
+    arg_type: Box<LocTypeLiteral>
 }
 
 pub enum TypeLiteral {
     Generic(String),
+    Void,
     Int32,
     Int64,
     Bool,
@@ -28,6 +113,14 @@ pub enum TypeLiteral {
     Dict {
         keys: Box<LocTypeLiteral>,
         values: Box<LocTypeLiteral>
+    },
+    Callable {
+        generics: Vec<String>,
+        positional_arguments: Vec<LocTypeLiteral>,
+        variadic_argument: Box<Option<LocTypeLiteral>>,
+        keyword_arguments: Vec<KeywordArgumentTypeLiteral>,
+        keyword_variadic_argument: Box<Option<LocTypeLiteral>>,
+        return_type: Box<LocTypeLiteral>
     }
 }
 
@@ -138,6 +231,7 @@ pub struct Body {
 
 pub struct Argument {
     pub name: String,
+    pub arg_type: LocTypeLiteral,
     pub loc: Loc 
 }
 
@@ -153,7 +247,8 @@ pub struct FunctionPrototype {
     pub positional_arguments: Vec<Argument>,
     pub variadic_argument: Option<Argument>,
     pub keyword_arguments: Vec<KeywordArgument>,
-    pub keyword_variadic_argument: Option<Argument>
+    pub keyword_variadic_argument: Option<Argument>,
+    pub return_type: LocTypeLiteral
 }
 
 
