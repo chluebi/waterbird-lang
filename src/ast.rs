a041beb7b83dd33e704c6133497fc42ef17e5796
chluebi <42419603+chluebi@users.noreply.github.com>
2025-10-22 11:29:30 +0200

    extend initial ast to statements and functions

--- src/ast.rs
+++ src/ast.rs
@@ -1,8 +1,9 @@
 use std::ops::Range;
+use std::collections::HashMap;
 
-type Loc = Range<usize>;
+pub type Loc = Range<usize>;
 
-enum Type {
+pub enum Type {
     Generic(String),
     Int32,
     Int64,
@@ -16,7 +17,7 @@ enum Type {
     }
 }
 
-enum TypeLiteral {
+pub enum TypeLiteral {
     Generic(String),
     Int32,
     Int64,
@@ -30,14 +31,14 @@ enum TypeLiteral {
     }
 }
 
-struct LocTypeLiteral {
+pub struct LocTypeLiteral {
     expr: TypeLiteral,
     loc: Loc
 }
 
-enum BinOp {}
+pub enum BinOp {}
 
-enum UnOp {}
+pub enum UnOp {}
 
 pub struct CallArgument {
     pub expression: Box<LocExpr>,
@@ -52,7 +53,12 @@ pub struct CallKeywordArgument {
 }
 
 
-enum Expr {
+pub struct LambdaArgument {
+    pub name: String,
+    pub loc: Loc
+}
+
+pub enum Expr {
     Variable(String),
     Int32(i32),
     Int64(i64),
@@ -71,7 +77,7 @@ enum Expr {
         expr: Box<LocExpr>
     },
     FunctionCall {
-        function_name: String,
+        function_name: Box<LocExpr>,
         positional_arguments: Vec<CallArgument>,
         variadic_argument: Option<CallArgument>,
         keyword_arguments: Vec<CallKeywordArgument>,
@@ -80,10 +86,84 @@ enum Expr {
     Indexing {
         indexed: Box<LocExpr>,
         indexer: Box<LocExpr>
+    },
+    Lambda {
+       arguments: Vec<LambdaArgument>,
+       expr: Box<LocExpr>
     }
 }
 
-struct LocExpr {
+pub struct LocExpr {
     expr: Expr,
     loc: Loc
+}
+
+
+pub enum Stmt {
+    Assignment {
+        target: LocExpr,
+        expression: LocExpr,
+    },
+    FunctionCall {
+        expression: LocExpr
+    },
+    ListAppend {
+        target: LocExpr,
+        value: LocExpr
+    },
+    Return {
+        expression: LocExpr
+    },
+    IfElse {
+        condition: LocExpr,
+        if_body: Body,
+        else_body: Body
+    },
+    While {
+        condition: LocExpr,
+        body: Body
+    }
+}
+
+
+pub struct LocStmt {
+    pub statement: Stmt,
+    pub loc: Loc 
+}
+
+
+pub struct Body {
+    pub statements: Vec<LocStmt>
+}
+
+pub struct Argument {
+    pub name: String,
+    pub loc: Loc 
+}
+
+
+pub struct KeywordArgument {
+    pub name: String,
+    pub expression: LocExpr,
+    pub loc: Loc
+}
+
+
+pub struct FunctionPrototype {
+    pub positional_arguments: Vec<Argument>,
+    pub variadic_argument: Option<Argument>,
+    pub keyword_arguments: Vec<KeywordArgument>,
+    pub keyword_variadic_argument: Option<Argument>
+}
+
+
+pub struct Function {
+    pub name: String,
+    pub contract: FunctionPrototype,
+    pub body: Body,
+    pub loc: Loc 
+}
+
+pub struct Program {
+    pub functions: HashMap<String, Function>
 }
