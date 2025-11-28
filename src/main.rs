ea911d4acd54ad1d1b7bf27e09ecda9de7f7d273
chluebi <42419603+chluebi@users.noreply.github.com>
2025-11-28 20:55:55 +0100

    file to run as argument

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,15 @@ mod ast;
 mod interpreter;
 mod runner;
 
-
-
-
+use std::env;
 
 pub fn main() {
-    runner::run("programs/test_loop".to_string());
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <filename>", args[0]);
+        std::process::exit(1);
+    }
+    let filename = args[1].clone(); 
+
+    runner::run(filename);
 }
